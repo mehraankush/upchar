41376bc8cd79a3a990ae372d4550f25c8deea230
ankush <mehraankush36@gmail.com>
2023-02-17 05:44:20 +0530

    upchar

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ const app = express();
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static("public"));
 
-app.get("/",function(req,res){
+
+// for button 2
+app.get("/search",function(req,res){
     res.sendFile(__dirname + "/button2.html");
 });
 
-app.post("/", function(req,res){
+app.post("/search", function(req,res){
 
     let input_text = req.body.input;
     // const url= "https://script.google.com/macros/s/AKfycbwQ8n-kgYi3fSLHzCW50TMTuYzcf9ps3sCV37FZkGmqsFI6_GbO8a4n-YaXwtzjqyN6/exec";
@@ -23,16 +25,20 @@ app.post("/", function(req,res){
         res.sendFile(__dirname + "/heart_attack.html");
         // console.log("YAah! right");
     }
-    else if(input_text.includes("confusion") || input_text.includes("inability to talk") || input_text.includes("noisy breathing") || input_text.includes("loss of consciouness") || input_text.includes("choking") || input_text.includes("food stuck")){
+    else if(input_text.includes("confusion") || input_text.includes("inability to talk") || input_text.includes("noisy breathing") || input_text.includes("loss of consciouness") || input_text.includes("choking") || input_text.includes("food stuck") || input_text.includes("chocked") || input_text.includes("chocke") || input_text.includes("chock")){
         res.sendFile(__dirname + "/chocking.html");
         // console.log("YAah! right");
     }
     else if(input_text.includes("trouble speaking") || input_text.includes("weakness ") || input_text.includes("paralysis") || input_text.includes("problem seeing") || input_text.includes("severe headache") || input_text.includes("problem walking") || input_text.includes("stroke")){
         res.sendFile(__dirname + "/stroke.html");
-        // console.log("YAah! right");
+        // console.log("YAah! right"); 
     }
     else if(input_text.includes("shaking") || input_text.includes("dry mouth") || input_text.includes("breath lessness") || input_text.includes("feeling confusion") || input_text.includes("panic attack")){
         res.sendFile(__dirname + "/panic_attack.html");
+        // console.log("YAah! right");
+    }
+    else if(input_text.includes("bone") || input_text.includes("broken")  || input_text.includes("swealling") || input_text.includes("break") || input_text.includes("fracture") || input_text.includes("bluish")){
+        res.sendFile(__dirname + "/fracture.html");
         // console.log("YAah! right");
     }
     else if(input_text.includes("water") || input_text.includes("sea") || input_text.includes("drowning") || input_text.includes("river") || input_text.includes("pond") || input_text.includes("swimming") || input_text.includes("pool") || input_text.includes("sink") || input_text.includes("sinking") || input_text.includes("sank") || input_text.includes("drown") || input_text.includes("drowned")){
@@ -51,18 +57,12 @@ app.post("/", function(req,res){
         res.sendFile(__dirname + "/snake-bite.html");
         // console.log("YAah! right");
     }
-    else if(input_text.includes("fracture") || input_text.includes("bone") || input_text.includes("broken") || input_text.includes("pain")|| input_text.includes("swelling") || input_text.includes("bleeding")|| input_text.includes("bluish")){
-        res.sendFile(__dirname + "/fracture.html");
-        // console.log("YAah! right");
-    }
-    else if(input_text.includes("burns") || input_text.includes("heat") || input_text.includes("fire") || input_text.includes("flame")|| input_text.includes("burned")){
+   
+    else if(input_text.includes("burns") || input_text.includes("heat") || input_text.includes("fire") || input_text.includes("flame")|| input_text.includes("burned") || input_text.includes("burn")){
         res.sendFile(__dirname + "/burns.html");
         // console.log("YAah! right");
     }
-    else if(input_text.includes("chocking") || input_text.includes("chok") || input_text.includes("choked") || input_text.includes("food")|| input_text.includes("stucked")|| input_text.includes("choking")|| input_text.includes("neck")){
-        res.sendFile(__dirname + "/chocking.html");
-        // console.log("YAah! right");
-    }
+   
     else if(input_text.includes("diabetes") || input_text.includes("type1") || input_text.includes("type2") || input_text.includes("sugar")|| input_text.includes("prediabatic")|| input_text.includes("prediabatese")|| input_text.includes("urinating")|| input_text.includes("weight loss")|| input_text.includes("lossing weight")|| input_text.includes("diabetes")){
         res.sendFile(__dirname + "/diabetes.html");
         // console.log("YAah! right");
@@ -75,22 +75,40 @@ app.post("/", function(req,res){
         res.sendFile(__dirname + "/motion-sickness.html");
         // console.log("YAah! right");
     }
-    else if(input_text.includes("headache") || input_text.includes("head pain") || input_text.includes("exams")){
+    else if(input_text.includes("headache") || input_text.includes("head pain") || input_text.includes("exams")|| input_text.includes("head")){
         res.sendFile(__dirname + "/headache.html");
         // console.log("YAah! right");
     }
-    else if(input_text.includes("heart attact") || input_text.includes("chest pain") || input_text.includes("sweating") || input_text.includes("cardiac arrest")|| input_text.includes("heart")|| input_text.includes("chest")){
-        res.sendFile(__dirname + "/heart_attack.html");
+    else if(input_text.includes("asthma") || input_text.includes("shortness of breath") || input_text.includes(" chest tightness") || input_text.includes(" difficulty breathing")){
+        res.sendFile(__dirname + "/asthma.html");
         // console.log("YAah! right");
     }
+    
     else{
         console.log("Sorry! Can't Find Your Problem Please use the First-aid Brochure");
     }
 
-    // res.send("<a href = 'http://localhost:3000/'><button> Restart </button></a>");
+   
+});
+
+// For Home page
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/index.html");
+});
+app.post("/", function(req,res){
+    res.sendFile(__dirname + "/index.html")
+});
+
+// For 3rd button
+app.get("/brochure",function(req,res){
+    res.sendFile(__dirname + "/button3.html");
+});
+app.post("/brochure", function(req,res){
+    res.sendFile(__dirname + "/button3.html")
 });
 
 
-app.listen(8000, function(){
+
+app.listen(3000, function(){
     console.log("The server is running at port 3000");
 });
